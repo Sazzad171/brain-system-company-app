ca66ed56fcc2fd2e3f7cd9d4ba5aa17254454105
sazzad.jafor@technonext.com <sazzad.jafor@technonext.com>
2025-11-18 00:41:08 +0600

    feat: iot solution page

---
+++ app/services/iot-solutions/page.tsx
@@ -0,0 +1,31 @@
+import CTA from "@/components/common/sections/cta"
+
+
+const IotSolutions = () => {
+  return (
+    <>
+      {/* banner */}
+
+      {/* benefit */}
+
+      {/* life cycle */}
+
+      {/* challenge banner */}
+
+      {/* tech stack */}
+
+      {/* we work with */}
+
+      {/* device layer */}
+
+      {/* why choose */}
+
+      {/* driving business */}
+
+      {/* cta */}
+      <CTA/>
+    </>
+  )
+}
+
+export default IotSolutions
