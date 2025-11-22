bac6c3aa5416b1703c2cad7a057aebaff9981f46
sazzad.jafor@technonext.com <sazzad.jafor@technonext.com>
2025-11-23 00:32:58 +0600

    feat: web-apps page ui

--- app/services/web-apps/page.tsx
+++ app/services/web-apps/page.tsx
@@ -3,6 +3,32 @@ import CTA from '@/components/common/sections/cta';
 const WebApps = () => {
   return (
     <>
+      {/* banner */}
+
+      {/* clients */}
+
+      {/* experienced web design */}
+
+      {/* building websites */}
+
+      {/* our web dev */}
+
+      {/* visitors banner */}
+
+      {/* user friendly web */}
+
+      {/* technically flawless */}
+
+      {/* structural choices */}
+
+      {/* optiized search */}
+
+      {/* your expectation */}
+
+      {/* portfolio */}
+
+      {/* faq */}
+
       {/* cta */}
       <CTA maxWidth="lg" isDetails />
     </>
